3bc5c705eec1c5138587eb04aa10e2954756d0fd
MAROUANE-ED-DAHRAOUI <dahraouimarouan@gmail.com>
2024-11-22 11:02:32 +0100

    UPD

---
+++ Module00/ex02/Account.cpp
@@ -0,0 +1,30 @@
+#include "Account.hpp"
+#include <string>
+
+static int	getNbAccounts( void )
+{
+    return (_nbAccounts);
+}
+
+static int	getTotalAmount( void )
+{
+    return (_totalAmount);
+}
+
+static int	getNbDeposits( void )
+{
+    return (_totalNbDeposits);
+}
+
+static int	getNbWithdrawals( void )
+{
+    return (_nbWithdrawals);
+}
+
+static void	displayAccountsInfos( void )
+{
+    std::cout << "";
+    std::cout << "";
+    std::cout << "";
+    std::cout << "";
+}
