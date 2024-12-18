f96c42e68fa401652c3b84d78c42f5f50e26ac32
Marouane Ed Dahraoui <med-dahr@c2r9p3.1337.ma>
2024-12-18 12:15:18 +0100

    feat: Extend Fixed class with operator overloading and utility functions - Overloaded comparison operators: >, <, >=, <=, ==, !=. - Overloaded arithmetic operators: +, -, *, /. - Implemented pre/post increment and decrement operators. - Added static member functions min and max for both constant and non-constant references. - Thoroughly tested all new features in main.cpp.

--- Module02/ex02/main.cpp
+++ Module02/ex02/main.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: med-dahr <med-dahr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/18 11:39:53 by med-dahr          #+#    #+#             */
+/*   Updated: 2024/12/18 12:13:41 by med-dahr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "fixed.hpp"
 #include <iostream>
 
@@ -6,13 +18,13 @@ int main( void )
     Fixed a;
     Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
         
-    std::cout << a << std::endl;
-    std::cout << ++a << std::endl;
-    std::cout << a << std::endl;
-    std::cout << a++ << std::endl;
-    std::cout << a << std::endl;
-    std::cout << b << std::endl;
-    std::cout << Fixed::max( a, b ) << std::endl;
+    std::cout << GREEN << a << RESET << std::endl;
+    std::cout << GREEN << ++a << RESET << std::endl;
+    std::cout << GREEN << a << RESET << std::endl;
+    std::cout << GREEN << a++ << RESET << std::endl;
+    std::cout << GREEN << a << RESET << std::endl;
+    std::cout << GREEN << b << RESET << std::endl;
+    std::cout << GREEN << Fixed::max( a, b ) << std::endl;
     
     return 0;
 }
