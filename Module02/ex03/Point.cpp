1c114e4b645c70ff7dc8c796325416667b6a85ee
MAROUANE-ED-DAHRAOUI <dahraouimarouan@gmail.com>
2024-12-22 13:52:17 +0100

    feat: Implement BSP algorithm to determine if a point is inside a triangle - Added Point class in Orthodox Canonical Form with private Fixed attributes  and . - Implemented  function to check if a point lies inside a triangle using the area method. - Added helper function  to compute the absolute area of a triangle formed by three points. - Included tests to validate the correctness of the bsp function with various edge cases. - Ensured clean and efficient code with proper separation of concerns and file structure.

--- Module02/ex03/Point.cpp
+++ Module02/ex03/Point.cpp
@@ -11,14 +11,14 @@ Point::Point(const float xnb, const float ynb) : _x(Fixed(xnb)), _y(Fixed(ynb))
 
 }
 
-Point::Point(const Point &Ob) : _x(Ob._x), _y(Ob._y)
+Point::Point(const Point &Ob) : _x(Fixed(Ob._x)), _y(Fixed(Ob._y))
 {
 
 }
 
 Point& Point::operator=(const Point &Ob)
 {
-    (void)Ob;
+    (void) Ob;
     return *this;
 }
 
@@ -29,10 +29,10 @@ Point::~Point()
 
 Fixed Point::getX() const
 {
-    return (_x);
+    return (this->_x);
 }
 
 Fixed Point::getY() const
 {
-    return (_y);
+    return (this->_y);
 }
