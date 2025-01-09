79721195aff5f91e885502f6a054b3b1d3b50e7e
MAROUANE-ED-DAHRAOUI <dahraouimarouan@gmail.com>
2025-01-09 17:57:17 +0100

    IS Cooking hmm

--- Module03/ClapTrap.cpp
+++ Module03/ClapTrap.cpp
@@ -1,34 +1,75 @@
 #include "ClapTrap.hpp"
 
-ClapTrap::ClapTrap() : Hit(10), Energy(10), AttackDamage(0)
+ClapTrap::ClapTrap() : Name("ClapTrap"), HitPoint(10), EnergyPoint(10), AttackDamage(0)
 {
-
+    std::cout << GREEN <<"default constructor call" << RESET << std::endl;
 }
 
-ClapTrap::ClapTrap(const std::string name)
+ClapTrap::ClapTrap(const std::string name) : HitPoint(10), EnergyPoint(10), AttackDamage(0)
 {
-    std::cout << "constructor parameters call";
+    std::cout << GREEN << "constructor parameters call" << RESET << std::endl;
     Name = name;
 }
 
-ClapTrap::ClapTrap(ClapTrap &name) : Hit(10), Energy(10), AttackDamage(0)
+ClapTrap::ClapTrap(ClapTrap &name) 
 {
-    std::cout << "copy constructor Is call \n";
+    std::cout << GREEN << "copy constructor Is call \n" << RESET;
+    *this = name;
+}
+
+ClapTrap& ClapTrap::operator=(const ClapTrap& name)
+{
     if(this != &name)
     {
         Name = name.Name;
-        Hit = name.Hit;
-        Energy = name.Energy;
+        HitPoint = name.HitPoint;
+        EnergyPoint = name.EnergyPoint;
         AttackDamage = name.AttackDamage;
     }
+    return (*this);
 }
 
 ClapTrap::~ClapTrap()
 {
-    std::cout << "destructor is call" << std::endl;
+    std::cout << GREEN << "destructor is call" << RESET << std::endl;
 }
 
 void   ClapTrap::attack(const std::string &target)
 {
-    std::cout << "";
+    if(HitPoint > 0 && EnergyPoint > 0)
+    {
+            std::cout << "ClapTrap " << Name << " Attacks " << target << " causing " << AttackDamage << " points of domage!" << std::endl;
+            EnergyPoint--;
+    }
+    else
+        std::cout << RED << "ClapTrap can't do anything! \n" << RESET;
+}
+
+void ClapTrap::takeDamage(unsigned int amount)
+{
+    if(HitPoint > 0 && EnergyPoint > 0)
+    {
+        std::cout << YELLOW <<"ClapTrap " << RESET << BOLD << Name 
+        << RESET << YELLOW << " take " << RESET << BOLD << amount << RESET
+        << YELLOW << " of points" << RESET << std::endl;
+        HitPoint -= amount;
+        AttackDamage -= amount;
+    }
+    else
+        std::cout << "ClapTrap can't do anything!" << std::endl;
+}
+
+void ClapTrap::beRepaired(unsigned int amount)
+{
+    if(HitPoint > 0 && EnergyPoint > 0)
+    {
+        std::cout << YELLOW << "ClapTrap " <<  RESET << Name << YELLOW << " repairs itself, restoring " <<  RESET << BOLD << amount << RESET 
+            << YELLOW << " points of damage" RESET << std::endl;
+        HitPoint += amount;
+        AttackDamage += amount;
+    }
+    else
+    {
+        std::cout << RED << "ClapTrap can't do anything" << RESET << std::endl;
+    }
 }
--- Module03/ClapTrap.hpp
+++ Module03/ClapTrap.hpp
@@ -4,17 +4,26 @@
 #include <iostream>
 #include <string>
 
+
+#define RED         "\033[31m"      // Red text
+#define GREEN       "\033[32m"      // Green text
+#define YELLOW      "\033[33m"      // Yellow text
+#define BOLD        "\033[1m"       // Bold text
+#define MAGENTA     "\033[35m"
+#define RESET       "\033[0m"
+
 class ClapTrap
 {
     private:
         std::string Name;
-        int         Hit;
-        int         Energy;
+        int         HitPoint;
+        int         EnergyPoint;
         int         AttackDamage;
     public:
         ClapTrap();
         ClapTrap(ClapTrap &claptrap);
         ClapTrap(const std::string _Name);
+        ClapTrap&  operator=(const ClapTrap &name);
         ~ClapTrap();
 
         void attack(const std::string& target);
